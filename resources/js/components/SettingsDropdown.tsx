6b0d68e696ba3a87ae2d04597f505d45878e9bfc
Alex Vanderbist <alex.vanderbist@gmail.com>
2022-01-05 11:10:51 +0100

    Add note about saving to dotfile to settings dialog

--- resources/js/components/SettingsDropdown.tsx
+++ resources/js/components/SettingsDropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IgnitionConfigContext, IgnitionIcon } from '@flareapp/ignition-ui';
+import { IgnitionConfigContext, IgnitionIcon, InlineCodeSnippet } from '@flareapp/ignition-ui';
 import { useContext } from 'react';
 import { IgniteDataContext } from 'contexts/IgniteDataContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -104,7 +104,7 @@ export default function SettingsDropdown({ isOpen }: Props) {
             <div className="flex px-4 justify-end">
                 <div className="w-0 h-0 border-[10px] border-t-0 border-transparent ~border-b-dropdown" />
             </div>
-            <div className="~bg-dropdown px-10 py-8 shadow-2xl">
+            <div className="flex flex-col gap-6 ~bg-dropdown px-10 py-8 shadow-2xl">
                 <div className="flex items-center justify-between gap-6">
                     <h4 className="whitespace-nowrap font-semibold">Ignition Settings</h4>
                     <a
@@ -115,34 +115,45 @@ export default function SettingsDropdown({ isOpen }: Props) {
                         <IgnitionIcon />
                     </a>
                 </div>
-                <h4 className="mt-6 uppercase tracking-wider ~text-gray-500 text-xs font-bold">Editor</h4>
-                <div className="group mt-2 relative">
-                    <select
-                        className="block appearance-none w-full ~bg-gray-500/5 h-12 px-4 pr-8 rounded-none leading-tight"
-                        value={editor}
-                        onChange={(event) => handleEditorChange(event.target.value)}
-                    >
-                        {Object.entries(ignitionConfig.editorOptions).map(([editor, { label }]) => (
-                            <option key={editor} value={editor}>
-                                {label}
-                            </option>
-                        ))}
-                    </select>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4">
-                        <FontAwesomeIcon icon={faAngleDown} className="group-hover:text-indigo-500 text-sm" />
+
+                <label htmlFor="editor-select">
+                    <span className="uppercase tracking-wider ~text-gray-500 text-xs font-bold">Editor</span>
+                    <div className="group mt-2 relative">
+                        <select
+                            id="editor-select"
+                            className="block appearance-none w-full ~bg-gray-500/5 h-12 px-4 pr-8 rounded-none leading-tight"
+                            value={editor}
+                            onChange={(event) => handleEditorChange(event.target.value)}
+                        >
+                            {Object.entries(ignitionConfig.editorOptions).map(([editor, { label }]) => (
+                                <option key={editor} value={editor}>
+                                    {label}
+                                </option>
+                            ))}
+                        </select>
+                        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4">
+                            <FontAwesomeIcon icon={faAngleDown} className="group-hover:text-indigo-500 text-sm" />
+                        </div>
                     </div>
-                </div>
-                <h4 className="mt-6 uppercase tracking-wider ~text-gray-500 text-xs font-bold">Theme</h4>
-                <button className="mt-2 w-full ~bg-gray-500/5 rounded-none leading-tight" onClick={handleThemeChange}>
-                    <div
-                        className="group flex items-center"
-                        style={{ WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 100%)' }}
+                </label>
+
+                <div>
+                    <span className="uppercase tracking-wider ~text-gray-500 text-xs font-bold">Theme</span>
+                    <button
+                        className="mt-2 w-full ~bg-gray-500/5 rounded-none leading-tight"
+                        onClick={handleThemeChange}
                     >
-                        <div className="px-4">
-                            {themeOptions.map(({ icon, value, selected }) => (
-                                <div
-                                    key={value}
-                                    className={`
+                        <div
+                            className="group flex items-center"
+                            style={{
+                                WebkitMaskImage: 'linear-gradient(to bottom, rgba(0,0,0,1) 60%, rgba(0,0,0,0) 100%)',
+                            }}
+                        >
+                            <div className="px-4">
+                                {themeOptions.map(({ icon, value, selected }) => (
+                                    <div
+                                        key={value}
+                                        className={`
                                         h-12 flex items-center origin-bottom
                                         ${selected ? 'transition-transform duration-1000' : ''}
                                         ${
@@ -156,19 +167,21 @@ export default function SettingsDropdown({ isOpen }: Props) {
                                                 : ''
                                         }
                                     `}
-                                >
-                                    <span className={`text-sm ~text-gray-500 transition-colors duration-500`}>
-                                        {icon}
-                                    </span>
-                                </div>
-                            ))}
+                                    >
+                                        <span className={`text-sm ~text-gray-500 transition-colors duration-500`}>
+                                            {icon}
+                                        </span>
+                                    </div>
+                                ))}
+                            </div>
+                            <div id="theme-name" className="-ml-1 first-letter:uppercase">
+                                {themeOptions.find((t) => t.selected)?.value}
+                            </div>
                         </div>
-                        <div id="theme-name" className="-ml-1 first-letter:uppercase">
-                            {themeOptions.find((t) => t.selected)?.value}
-                        </div>
-                    </div>
-                </button>
-                <div className="mt-6 flex items-center gap-4">
+                    </button>
+                </div>
+
+                <div className="flex items-center gap-4">
                     <button
                         onClick={updateConfig}
                         disabled={isUpdatingConfig}
@@ -187,6 +200,9 @@ export default function SettingsDropdown({ isOpen }: Props) {
                     </button>
                     {updateWasSuccessful && <p className="text-emerald-500 text-sm">Saved!</p>}
                 </div>
+                <p className="text-xs">
+                    Settings saved will be saved locally in <InlineCodeSnippet>~/.ignition.json</InlineCodeSnippet>.
+                </p>
             </div>
         </div>
     );
