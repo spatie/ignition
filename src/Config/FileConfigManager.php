16756d86dae251bb1d6c7ad50e3dfe4f74f09535
Sergey Kudashev <kudashevs@gmail.com>
2022-02-16 11:26:10 +0300

    Update Config add FileConfigManager class with basic realization.

---
+++ src/Config/FileConfigManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Spatie\Ignition\Config;
+
+use Spatie\Ignition\Contracts\ConfigManager;
+
+class FileConfigManager implements ConfigManager
+{
+    private const SETTINGS_FILE_NAME = '.ignition.json';
+
+    private string $path;
+
+    public function save(): bool
+    {
+        return true;
+    }
+
+    public function load(): array
+    {
+        return [];
+    }
+
+    public function getSource(): array
+    {
+        return [
+            'name' => self::SETTINGS_FILE_NAME,
+            'path' => $this->path,
+            'file' => $this->generateFullFilePath(),
+        ];
+    }
+
+    public function updateSource(array $settings): bool
+    {
+        if (!array_key_exists('path', $settings)) {
+            return false;
+        }
+
+        if ($path = $this->initPath($settings['path'])) {
+            $this->path = rtrim($path, DIRECTORY_SEPARATOR);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function initPath(string $path): string
+    {
+        $path = realpath($path);
+
+        if ($this->isValidPath($path)) {
+            return $path;
+        }
+
+        return '';
+    }
+
+    private function isValidPath(string $path): bool
+    {
+        return ($path !== '') && file_exists($path) && is_writable($path);
+    }
+
+    public function createSource(): bool
+    {
+        return true;
+    }
+
+    private function generateFullFilePath(): string
+    {
+        return $this->path . DIRECTORY_SEPARATOR . self::SETTINGS_FILE_NAME;
+    }
+}
