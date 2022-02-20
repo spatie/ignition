9996fcef285f1adecfe39dd8f4e5d5cded4a0dcd
Sergey Kudashev <kudashevs@gmail.com>
2022-02-20 21:29:32 +0300

    Update FileConfigManagerTest update title of check can load from a file test.

--- tests/Config/FileConfigManagerTest.php
+++ tests/Config/FileConfigManagerTest.php
@@ -84,7 +84,7 @@ test('the file config manager can save to a config file created from a filepath'
     $this->assertStringContainsString('saved', file_get_contents($configSource['file']));
 });
 
-test('the file config manager can load from the config file from a filepath', function () {
+test('the file config manager can load from a config file created from a filepath', function () {
     $configManager = new FileConfigManager(SETTINGS_FILE_DIRECTORY);
     $configManager->save([
         'test' => 'saved',
