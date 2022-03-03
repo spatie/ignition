4a72c3e3dd2afb07ffd45d24f12f2408ad6f3978
Sergey Kudashev <kudashevs@gmail.com>
2022-03-03 10:15:50 +0300

    Refactor FileConfigManagerTest change test function to it function in tests.

--- tests/Config/FileConfigManagerTest.php
+++ tests/Config/FileConfigManagerTest.php
@@ -8,7 +8,7 @@ afterEach(function () {
     removeTempSettingsFile();
 });
 
-test('the file config manager can use a default file in Unix-like environment', function () {
+it('can use a default file in Unix-like environment', function () {
     putenv('HOME=' . realpath(SETTINGS_FILE_DIRECTORY));
 
     $configManager = new FileConfigManager();
@@ -19,7 +19,7 @@ test('the file config manager can use a default file in Unix-like environment', 
     $this->assertFileExists($configSource['file']);
 })->skip(fn () => isWindows() === true, 'This test can be run only in the non-Windows environment.');
 
-test('the file config manager can use a default file in Windows environment', function () {
+it('can use a default file in Windows environment', function () {
     [$disk, $path] = explode(':', realpath(SETTINGS_FILE_DIRECTORY), 2);
 
     $_SERVER['HOMEDRIVE'] = "{$disk}:";
@@ -33,7 +33,7 @@ test('the file config manager can use a default file in Windows environment', fu
     $this->assertFileExists($configSource['file']);
 })->skip(fn () => isWindows() === false, 'This test can be run only in the Windows environment.');
 
-test('the file config manager cannot process an empty filepath', function () {
+it('cannot process an empty filepath', function () {
     $configManager = new FileConfigManager('   ');
     $configSource = $configManager->getPersistentInfo();
 
@@ -41,7 +41,7 @@ test('the file config manager cannot process an empty filepath', function () {
     $this->assertEmpty($configSource['file']);
 });
 
-test('the file config manager cannot process a wrong filepath', function () {
+it('cannot process a wrong filepath', function () {
     $configManager = new FileConfigManager(__DIR__ . '/wrong_file_path');
     $configSource = $configManager->getPersistentInfo();
 
@@ -49,7 +49,7 @@ test('the file config manager cannot process a wrong filepath', function () {
     $this->assertEmpty($configSource['file']);
 });
 
-test('the file config manager can process a filepath', function () {
+it('can process a filepath', function () {
     $configManager = new FileConfigManager(SETTINGS_FILE_DIRECTORY);
     $configSource = $configManager->getPersistentInfo();
 
@@ -57,7 +57,7 @@ test('the file config manager can process a filepath', function () {
     $this->assertStringContainsString('temp', $configSource['path']);
 });
 
-test('the file config manager can create a config file from a filepath', function () {
+it('can create a config file from a filepath', function () {
     $configManager = new FileConfigManager(SETTINGS_FILE_DIRECTORY);
 
     $configSource = $configManager->getPersistentInfo();
@@ -66,7 +66,7 @@ test('the file config manager can create a config file from a filepath', functio
     $this->assertFileExists($configSource['file']);
 });
 
-test('the file config manager can save to a config file created from a filepath', function () {
+it('can save to a config file created from a filepath', function () {
     $configManager = new FileConfigManager(SETTINGS_FILE_DIRECTORY);
     $configManager->save([
         'test' => 'saved',
@@ -79,7 +79,7 @@ test('the file config manager can save to a config file created from a filepath'
     $this->assertStringContainsString('saved', file_get_contents($configSource['file']));
 });
 
-test('the file config manager can load from a config file created from a filepath', function () {
+it('can load from a config file created from a filepath', function () {
     $configManager = new FileConfigManager(SETTINGS_FILE_DIRECTORY);
     $configManager->save([
         'test' => 'saved',
