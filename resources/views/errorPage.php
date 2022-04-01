eef3735deff9772f2087035b7af56a539928dbcd
Willem Van Bockstal <willem@spatie.be>
2022-04-01 13:59:12 +0200

    Add background and text colors for

--- resources/views/errorPage.php
+++ resources/views/errorPage.php
@@ -19,14 +19,15 @@
 
     <title><?= $viewModel->title() ?></title>
 
-    <style><?= $viewModel->getAssetContents('ignition.css') ?></style>
-
     <script>
         // Process `auto` theme as soon as possible to avoid flashing of white background:
         if (document.documentElement.classList.contains('auto') && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             document.documentElement.classList.add('dark');
         }
-    </script>    
+    </script> 
+       
+    <style><?= $viewModel->getAssetContents('ignition.css') ?></style>
+
 </head>
 <body class="scrollbar-lg">
 
