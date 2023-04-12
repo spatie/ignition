055baabe83761ef1441d5a4fde2c602bc2fb5c2b
Freek Van der Herten <freek@spatie.be>
2023-04-12 10:43:21 +0200

    wip

--- src/Solutions/OpenAi/DummyCache.php
+++ src/Solutions/OpenAi/DummyCache.php
@@ -13,18 +13,22 @@ class DummyCache implements CacheInterface
 
     public function set(string $key, mixed $value, \DateInterval|int|null $ttl = null): bool
     {
+        return true;
     }
 
     public function delete(string $key): bool
     {
+        return true;
     }
 
     public function clear(): bool
     {
+        return true;
     }
 
     public function getMultiple(iterable $keys, mixed $default = null): iterable
     {
+        return [];
     }
 
     public function setMultiple(iterable $values, \DateInterval|int|null $ttl = null): bool
