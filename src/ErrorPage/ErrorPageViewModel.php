2bb05b0d571347d364f3000bc762a43bb25f170d
rubenvanassche <rubenvanassche@users.noreply.github.com>
2024-06-12 10:12:12 +0000

    Fix styling

--- src/ErrorPage/ErrorPageViewModel.php
+++ src/ErrorPage/ErrorPageViewModel.php
@@ -2,11 +2,11 @@
 
 namespace Spatie\Ignition\ErrorPage;
 
+use Spatie\ErrorSolutions\Contracts\Solution;
+use Spatie\ErrorSolutions\Solutions\SolutionTransformer;
 use Spatie\FlareClient\Report;
 use Spatie\FlareClient\Truncation\ReportTrimmer;
 use Spatie\Ignition\Config\IgnitionConfig;
-use Spatie\ErrorSolutions\Contracts\Solution;
-use Spatie\ErrorSolutions\Solutions\SolutionTransformer;
 use Throwable;
 
 class ErrorPageViewModel
